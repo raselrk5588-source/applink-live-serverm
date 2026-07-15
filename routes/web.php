b0ecf02508e78190f8cb62e3b160e9c4645613db
tishaakter0r-beep <tishaakter0r@gmail.com>
2026-07-15 12:29:46 +0600

    Fix API and Web route closures for route caching

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', function () {
-    return redirect()->route("login");
-});
+Route::redirect('/', '/login');
 
 Auth::routes();
 
